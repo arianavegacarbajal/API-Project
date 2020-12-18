422e1d7e07573724f2ee6883d47e1e8ed6a13d75
Ariana Vega Carbajal <arivegacarbajal@arianas-mbp.attlocal.net>
2020-12-17 22:31:51 -0500

    api

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-let url = 'https://picsum.photos/200';
+let url = 'https://picsum.photos/600/300';
 
-document.getElementsByClassName
+
+function pictureFetcher(){
+fetch(url)
+.then(function(result){
+    console.log(result)
+    displayResults(result);
+})
+}
+
+function displayResults(result){
+    const section = document.getElementById('image')
+    while (section.firstChild) {
+        section.removeChild(section.firstChild);
+    }
+    let img = document.createElement('img');
+    img.src = (result.url);
+    document.getElementById('image').appendChild(img);
+}
+
+let button1 = document.querySelector('#button1');
+
+
+let numberOfButtons = document.querySelectorAll('.button')
+for (let i = 0; i < numberOfButtons.length; i++){
+    numberOfButtons[i].addEventListener('click', pictureFetcher)
+}
